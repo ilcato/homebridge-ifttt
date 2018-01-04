c7a7b2953269108448efd3788e302b9c97aac083
Todd Greenfield <todd.greenfield@me.com>
2018-01-04 17:58:29 -0500

    Fixed indents

--- index.js
+++ index.js
@@ -80,9 +80,9 @@ var request = require("request");
 
 
 function IFTTTPlatform(log, config){
-  	this.log          = log;
-  	this.makerkey     = config["makerkey"];
-  	this.IFTTTaccessories = config["accessories"];
+  this.log          = log;
+  this.makerkey     = config["makerkey"];
+  this.IFTTTaccessories = config["accessories"];
 }
 
 module.exports = function(homebridge) {
@@ -94,34 +94,34 @@ module.exports = function(homebridge) {
 
 IFTTTPlatform.prototype = {
   accessories: function(callback) {
-      this.log("Loading accessories...");
+    this.log("Loading accessories...");
 
-      var that = this;
-      var foundAccessories = [];
-      if (this.IFTTTaccessories == null || this.IFTTTaccessories.length == 0) {
-      	callback(foundAccessories);
-      	return;
-      }
+    var that = this;
+    var foundAccessories = [];
+    if (this.IFTTTaccessories == null || this.IFTTTaccessories.length == 0) {
+      callback(foundAccessories);
+      return;
+    }
 
-      this.cacheDirectory = HomebridgeAPI.user.persistPath();
-      this.storage = require('node-persist');
-      this.storage.initSync({dir:this.cacheDirectory, forgiveParseErrors: true});
+    this.cacheDirectory = HomebridgeAPI.user.persistPath();
+    this.storage = require('node-persist');
+    this.storage.initSync({dir:this.cacheDirectory, forgiveParseErrors: true});
 
-	  this.IFTTTaccessories.map(function(s) {
-		that.log("Found: " + s.name);
-			var accessory = null;
-			if (s.buttons.length != 0) {
-				var services = [];
-				for (var b = 0; b < s.buttons.length; b++) {
-					var service = {
-						controlService: new Service.Switch(s.buttons[b].caption),
-						characteristics: [Characteristic.On]
-					};
-					service.controlService.subtype = s.buttons[b].trigger != null ? s.buttons[b].trigger : s.buttons[b].triggerOn + s.buttons[b].triggerOff;
-					service.controlService.trigger = s.buttons[b].trigger;
-					service.controlService.triggerOn = s.buttons[b].triggerOn;
-					service.controlService.triggerOff = s.buttons[b].triggerOff;
-					service.controlService.delayOn = s.buttons[b].delayOn;
+    this.IFTTTaccessories.map(function(s) {
+      that.log("Found: " + s.name);
+      var accessory = null;
+      if (s.buttons.length != 0) {
+        var services = [];
+        for (var b = 0; b < s.buttons.length; b++) {
+          var service = {
+            controlService: new Service.Switch(s.buttons[b].caption),
+            characteristics: [Characteristic.On]
+          };
+          service.controlService.subtype = s.buttons[b].trigger != null ? s.buttons[b].trigger : s.buttons[b].triggerOn + s.buttons[b].triggerOff;
+          service.controlService.trigger = s.buttons[b].trigger;
+          service.controlService.triggerOn = s.buttons[b].triggerOn;
+          service.controlService.triggerOff = s.buttons[b].triggerOff;
+          service.controlService.delayOn = s.buttons[b].delayOn;
           service.controlService.delayOff = s.buttons[b].delayOff;
           service.controlService.stateful = s.buttons[b].stateful;
           // Read saved state and set initial
@@ -138,140 +138,140 @@ IFTTTPlatform.prototype = {
             that.log("Loading service: " + service.controlService.displayName + ", subtype: " + service.controlService.subtype);
           }
           services.push(service);
-				}
-				accessory = new IFTTTAccessory(services);
-			}
-			if (accessory != null) {
-				accessory.getServices = function() {
-						return that.getServices(accessory);
-				};
-				accessory.platform 			= that;
-				accessory.remoteAccessory	= s;
-				accessory.name				= s.name;
-				accessory.model				= "IFTTT";
-				accessory.manufacturer		= "IFTTT";
-				accessory.serialNumber		= "<unknown>";
-				foundAccessories.push(accessory);
-
-			}
-		}
-  )
-      callback(foundAccessories);
-  },
-  command: function(c,value, that) {
-    var url = "https://maker.ifttt.com/trigger/"+c+"/with/key/"+this.makerkey;
-	var method = "get";
-    request({
-	    url: url,
-		method: method
-    }, function(err, response) {
-      if (err) {
-        that.platform.log("There was a problem sending command " + c + " to" + that.name);
-        that.platform.log(url);
-      } else {
-        that.platform.log(that.name + " sent command " + c);
-        that.platform.log(url);
+        }
+        accessory = new IFTTTAccessory(services);
       }
-    });
-  },
-  getInformationService: function(homebridgeAccessory) {
-    var informationService = new Service.AccessoryInformation();
-    informationService
-                .setCharacteristic(Characteristic.Name, homebridgeAccessory.name)
-				.setCharacteristic(Characteristic.Manufacturer, homebridgeAccessory.manufacturer)
-			    .setCharacteristic(Characteristic.Model, homebridgeAccessory.model)
-			    .setCharacteristic(Characteristic.SerialNumber, homebridgeAccessory.serialNumber);
-  	return informationService;
-  },
-  bindCharacteristicEvents: function(characteristic, service, homebridgeAccessory) {
-  	var onOff = characteristic.props.format == "bool" ? true : false;
-    	characteristic
-		.on('set', function(value, callback, context) {
-            if (context !== 'fromSetValue') {
-							var trigger = null;
-							var delayOn = service.controlService.delayOn;
-							var delayOff = service.controlService.delayOff;
-							if (service.controlService.trigger != null)
-								trigger = service.controlService.trigger;
-							else if (value == 0) {
-								trigger = service.controlService.triggerOff;
-								service.controlService.onoffstate = false;
-							} else {
-								trigger = service.controlService.triggerOn;
-								service.controlService.onoffstate = true;
-							}
+      if (accessory != null) {
+        accessory.getServices = function() {
+          return that.getServices(accessory);
+        };
+        accessory.platform 			= that;
+        accessory.remoteAccessory	= s;
+        accessory.name				= s.name;
+        accessory.model				= "IFTTT";
+        accessory.manufacturer		= "IFTTT";
+        accessory.serialNumber		= "<unknown>";
+        foundAccessories.push(accessory);
 
-							if(shouldDelayCommand(value, delayOn, delayOff)){
-								homebridgeAccessory.platform.log(trigger + " scheduled to run in " + getDelay(value, delayOn, delayOff)/1000 + " seconds.");
-								setTimeout(function() {
-									homebridgeAccessory.platform.command(trigger, "", homebridgeAccessory);
-								}, getDelay(value, delayOn, delayOff));
-							} else {
-								homebridgeAccessory.platform.command(trigger, "", homebridgeAccessory);
-							}
-
-							if (service.controlService.trigger != null) {
-								// In order to behave like a push button reset the status to off
-								setTimeout( function(){
-									characteristic.setValue(false, undefined, 'fromSetValue');
-								}, 100 );
-							}
-              // Save state
-              if (service.controlService.stateful) {
-                this.storage.setItemSync(service.controlService.displayName, value);
-              }
-            }
-						callback();
-				   }.bind(this) );
-    characteristic
-        .on('get', function(callback) {
-        				if (service.controlService.trigger != null)
-							// a push button is normally off
-							callback(undefined, false);
-						else {
-							callback(undefined, service.controlService.onoffstate);
-						}
-                   }.bind(this) );
-  },
-  getServices: function(homebridgeAccessory) {
-  	var services = [];
-  	var informationService = homebridgeAccessory.platform.getInformationService(homebridgeAccessory);
-  	services.push(informationService);
-  	for (var s = 0; s < homebridgeAccessory.services.length; s++) {
-		var service = homebridgeAccessory.services[s];
-		for (var i=0; i < service.characteristics.length; i++) {
-			var characteristic = service.controlService.getCharacteristic(service.characteristics[i]);
-			if (characteristic == undefined)
-				characteristic = service.controlService.addCharacteristic(service.characteristics[i]);
-			homebridgeAccessory.platform.bindCharacteristicEvents(characteristic, service, homebridgeAccessory);
-		}
-		services.push(service.controlService);
+      }
     }
-    return services;
+  )
+  callback(foundAccessories);
+},
+command: function(c,value, that) {
+  var url = "https://maker.ifttt.com/trigger/"+c+"/with/key/"+this.makerkey;
+  var method = "get";
+  request({
+    url: url,
+    method: method
+  }, function(err, response) {
+    if (err) {
+      that.platform.log("There was a problem sending command " + c + " to" + that.name);
+      that.platform.log(url);
+    } else {
+      that.platform.log(that.name + " sent command " + c);
+      that.platform.log(url);
+    }
+  });
+},
+getInformationService: function(homebridgeAccessory) {
+  var informationService = new Service.AccessoryInformation();
+  informationService
+  .setCharacteristic(Characteristic.Name, homebridgeAccessory.name)
+  .setCharacteristic(Characteristic.Manufacturer, homebridgeAccessory.manufacturer)
+  .setCharacteristic(Characteristic.Model, homebridgeAccessory.model)
+  .setCharacteristic(Characteristic.SerialNumber, homebridgeAccessory.serialNumber);
+  return informationService;
+},
+bindCharacteristicEvents: function(characteristic, service, homebridgeAccessory) {
+  var onOff = characteristic.props.format == "bool" ? true : false;
+  characteristic
+  .on('set', function(value, callback, context) {
+    if (context !== 'fromSetValue') {
+      var trigger = null;
+      var delayOn = service.controlService.delayOn;
+      var delayOff = service.controlService.delayOff;
+      if (service.controlService.trigger != null)
+      trigger = service.controlService.trigger;
+      else if (value == 0) {
+        trigger = service.controlService.triggerOff;
+        service.controlService.onoffstate = false;
+      } else {
+        trigger = service.controlService.triggerOn;
+        service.controlService.onoffstate = true;
+      }
+
+      if(shouldDelayCommand(value, delayOn, delayOff)){
+        homebridgeAccessory.platform.log(trigger + " scheduled to run in " + getDelay(value, delayOn, delayOff)/1000 + " seconds.");
+        setTimeout(function() {
+          homebridgeAccessory.platform.command(trigger, "", homebridgeAccessory);
+        }, getDelay(value, delayOn, delayOff));
+      } else {
+        homebridgeAccessory.platform.command(trigger, "", homebridgeAccessory);
+      }
+
+      if (service.controlService.trigger != null) {
+        // In order to behave like a push button reset the status to off
+        setTimeout( function(){
+          characteristic.setValue(false, undefined, 'fromSetValue');
+        }, 100 );
+      }
+      // Save state
+      if (service.controlService.stateful) {
+        this.storage.setItemSync(service.controlService.displayName, value);
+      }
+    }
+    callback();
+  }.bind(this) );
+  characteristic
+  .on('get', function(callback) {
+    if (service.controlService.trigger != null)
+    // a push button is normally off
+    callback(undefined, false);
+    else {
+      callback(undefined, service.controlService.onoffstate);
+    }
+  }.bind(this) );
+},
+getServices: function(homebridgeAccessory) {
+  var services = [];
+  var informationService = homebridgeAccessory.platform.getInformationService(homebridgeAccessory);
+  services.push(informationService);
+  for (var s = 0; s < homebridgeAccessory.services.length; s++) {
+    var service = homebridgeAccessory.services[s];
+    for (var i=0; i < service.characteristics.length; i++) {
+      var characteristic = service.controlService.getCharacteristic(service.characteristics[i]);
+      if (characteristic == undefined)
+      characteristic = service.controlService.addCharacteristic(service.characteristics[i]);
+      homebridgeAccessory.platform.bindCharacteristicEvents(characteristic, service, homebridgeAccessory);
+    }
+    services.push(service.controlService);
   }
+  return services;
+}
 }
 
 function IFTTTAccessory(services) {
-    this.services = services;
+  this.services = services;
 }
 
 function shouldDelayCommand(value, delayOn, delayOff){
-	if(value === 1){
-		return delayOn;
-	} else if (value === 0){
-		return delayOff;
-	}
-	return false;
+  if(value === 1){
+    return delayOn;
+  } else if (value === 0){
+    return delayOff;
+  }
+  return false;
 }
 
 function getDelay(value, delayOn, delayOff){
-	var multiplier = 1000;
-	if(value === 1 && delayOn){
-		var result = delayOn * multiplier;
-		return result;
-	} else if (value === 0 && delayOff){
-		var result = delayOff * multiplier;
-		return result;
-	}
-	return 0;
+  var multiplier = 1000;
+  if(value === 1 && delayOn){
+    var result = delayOn * multiplier;
+    return result;
+  } else if (value === 0 && delayOff){
+    var result = delayOff * multiplier;
+    return result;
+  }
+  return 0;
 }
